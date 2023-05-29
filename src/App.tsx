9cee378886e8b6fd41eb2f12263d2a96b3c2da75
sasoft12 <91675998+sasoft12@users.noreply.github.com>
2023-05-29 01:12:07 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Perfect Work!
+          I am a senior Software Engineer.
         </p>
       </header>
     </div>
